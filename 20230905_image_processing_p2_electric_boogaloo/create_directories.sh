621473876472d7758db1c5fed99a931b4ff3bb61
mraiser <marc@raiser.us>
2023-09-05 11:11:08 -0400

    Fixed missing directories

--- 20230905_image_processing_p2_electric_boogaloo/create_directories.sh
+++ 20230905_image_processing_p2_electric_boogaloo/create_directories.sh
@@ -1,7 +1,10 @@
 mkdir faces
 mkdir good
 mkdir lora
-mkdir lora/body
-mkdir lora/face
+mkdir lora/Image
+mkdir lora/Image/body
+mkdir lora/Image/face
+mkdir lora/Model
+mkdir lora/Log
 mkdir match
 mkdir raw
